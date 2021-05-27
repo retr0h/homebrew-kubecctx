997a47afbb72f8530d379033c612027ffd55fba1
נυαη נυαηѕση <john@dewey.ws>
2021-05-27 10:53:54 -0700

    Updated with original brew-core PR

--- Formula/kubecctx.rb
+++ Formula/kubecctx.rb
@@ -1,11 +1,10 @@
 class Kubecctx < Formula
-  desc "A script that lets you manage multiple KUBECONF files"
+  desc "Script to manage multiple KUBECONF files"
   homepage "https://github.com/retr0h/kubecctx"
   url "https://github.com/retr0h/kubecctx/releases/download/0.1.0/v0.1.0.tar.gz"
   sha256 "91604e6b4ce63499b61ca79cd8f87550a22063cbaa5ea17f6aa7344d7aa02476"
   license "MIT"
   head "https://github.com/retr0h/kubecctx.git"
-  version "0.1.1"
 
   bottle :unneeded
 
@@ -24,7 +23,9 @@ class Kubecctx < Formula
 
   test do
     ENV["LC_CTYPE"] = "en_CA.UTF-8"
-    assert_equal "bash", shell_output("bash -c '. #{opt_share}/kubecctx.sh && echo $KUBE_PS1_SHELL'").chomp
-    assert_match "zsh", shell_output("zsh -c '. #{opt_share}/kubecctx.sh && echo $KUBE_PS1_SHELL'").chomp
+    assert_equal "bash",
+shell_output("bash -c '. #{opt_share}/kubecctx.sh && declare -F kubecctx &>/dev/null && echo bash'").chomp
+    assert_match "zsh",
+shell_output("zsh -c '. #{opt_share}/kubecctx.sh && declare -F kubecctx &>/dev/null && echo zsh'").chomp
   end
 end
